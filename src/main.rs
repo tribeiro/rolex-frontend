3aea21744935797da2bee53ea12b124fced6f2b2
Tiago Ribeiro <tribeiro@lsst.org>
2024-08-24 15:33:14 -0400

    Update main.rs.
    Add features to support static log explored (when users go directly to a single date), and add support for additional entries.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use axum::{
     routing::get,
     Router,
 };
-use rolex::{exposure_log::exposure_log, narrative_log::narrative_log};
+use rolex::{block_log::block_log, exposure_log::exposure_log, narrative_log::narrative_log};
+use serde::Deserialize;
 use std::collections::HashMap;
 use tower_http::services::ServeDir;
 
@@ -21,6 +22,7 @@ async fn main() -> anyhow::Result<()> {
     let app = Router::new()
         .nest("/api", api_router)
         .route("/", get(handler))
+        .route("/log_explorer_app", get(log_explorer_app))
         .route("/log_explorer", get(log_explorer))
         .route("/night_plan", get(night_plan))
         .route("/eon_report", get(eon_report))
@@ -36,12 +38,36 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+#[derive(Debug, Deserialize)]
+struct LogExplorerQuery {
+    log_date: Option<String>,
+}
+
 async fn handler() -> impl IntoResponse {
     let template = HelloTemplate {};
     HtmlTemplate(template)
 }
 
-async fn log_explorer() -> impl IntoResponse {
+async fn log_explorer(Query(query): Query<LogExplorerQuery>) -> impl IntoResponse {
+    println!("Log Date: {:?}", query.log_date);
+    let log_form = {
+        if let Some(selected_date) = &query.log_date {
+            let log_form = get_log_form(&selected_date).await;
+            log_form
+        } else {
+            LogForm {
+                logmessages: vec![],
+            }
+        }
+    };
+    let template = LogExplorerTemplateStatic {
+        selected_date: query.log_date.unwrap_or("Invalid Date".to_owned()),
+        logmessages: log_form.logmessages,
+    };
+    HtmlTemplate(template)
+}
+
+async fn log_explorer_app() -> impl IntoResponse {
     let template = LogExplorerTemplate {};
     HtmlTemplate(template)
 }
@@ -60,55 +86,100 @@ async fn get_log_messages(Query(params): Query<HashMap<String, String>>) -> impl
     println!("{params:?}");
 
     if let Some(selected_date) = params.get("log_date") {
-        let updated_content = format!("Content updated for date: {selected_date}");
-
-        println!("{updated_content}");
-
-        // let startDate: string = searchParams.get("log_date") + "T12:00:00";
-        // let endDate: string = dayjs(startDate).add(1, "day").format("YYYY-MM-DDTHH:mm:ss");
-
-        let parse_from_str = NaiveDateTime::parse_from_str;
-
-        match parse_from_str(&format!("{selected_date}T12:00:00"), "%Y-%m-%dT%H:%M:%S") {
-            Ok(min_date_added) => {
-                let max_date_added = min_date_added + Duration::days(1);
-                let base_url = "https://tucson-teststand.lsst.codes/narrativelog/messages";
-                let params = Some(HashMap::from([
-                    ("min_date_added".to_string(), min_date_added.to_string()),
-                    ("max_date_added".to_string(), max_date_added.to_string()),
-                    ("limit".to_string(), "10000".to_string()),
-                ]));
-
-                // let (narrative_logs, exposure_logs) = tokio::join!(
-                //     narrative_log::NarrativeLog::retrieve(base_url, &params),
-                //     narrative_log::NarrativeLog::retrieve(base_url, &params)
-                // );
-                if let Ok(narrative_logs) =
-                    narrative_log::NarrativeLog::retrieve(base_url, &params).await
-                {
-                    println!("number of messages: {}.", narrative_logs.len());
-                    let template = LogForm {
-                        logmessages: narrative_logs
-                            .into_iter()
-                            .map(|narrative_log| {
-                                narrative_log
-                                    .render()
-                                    .unwrap_or("Failed to render message.".to_string())
-                            })
-                            .collect(),
-                    };
-                    HtmlTemplate(template)
-                } else {
-                    get_empty_log_form()
-                }
-            }
-            Err(error) => {
-                println!("Error parsing selected date: {selected_date}. Err: {error}");
-                get_empty_log_form()
-            }
-        }
+        let template = get_log_form(&selected_date).await;
+        HtmlTemplate(template)
     } else {
         get_empty_log_form()
+    }
+}
+
+async fn get_log_form(selected_date: &str) -> LogForm {
+    let updated_content = format!("Content updated for date: {selected_date}");
+
+    println!("{updated_content}");
+
+    let parse_from_str = NaiveDateTime::parse_from_str;
+
+    match parse_from_str(&format!("{selected_date}T12:00:00"), "%Y-%m-%dT%H:%M:%S") {
+        Ok(min_date_added) => {
+            let max_date_added = min_date_added + Duration::days(1);
+            let params = Some(HashMap::from([
+                ("min_date_added".to_string(), min_date_added.to_string()),
+                ("max_date_added".to_string(), max_date_added.to_string()),
+                ("limit".to_string(), "10000".to_string()),
+            ]));
+
+            let base_url = "https://summit-lsp.lsst.codes/narrativelog/messages";
+            let narrative_logs = {
+                match narrative_log::NarrativeLog::retrieve(base_url, &params).await {
+                    Ok(narrative_log) => narrative_log,
+                    Err(error) => {
+                        println!("{error}");
+                        vec![]
+                    }
+                }
+            };
+
+            println!("Got {} narrative logs.", narrative_logs.len());
+            let base_url = "https://summit-lsp.lsst.codes/exposurelog/messages";
+            let exposure_logs = exposure_log::ExposureLog::retrieve(base_url, &params)
+                .await
+                .unwrap_or(vec![]);
+
+            println!("Got {} exposure logs.", exposure_logs.len());
+
+            let block_logs =
+                block_log::BlockLog::retrieve("summit_efd", &min_date_added, &max_date_added)
+                    .await
+                    .unwrap_or(vec![]);
+
+            let logmessages: Vec<(String, String)> = {
+                let mut logmessages: Vec<(String, String)> = narrative_logs
+                    .into_iter()
+                    .map(|entry| {
+                        (
+                            entry.get_date_added().to_string(),
+                            entry
+                                .render()
+                                .unwrap_or("Failed to render message.".to_string()),
+                        )
+                    })
+                    .chain(exposure_logs.into_iter().map(|entry| {
+                        (
+                            entry
+                                .get_date_added()
+                                .clone()
+                                .unwrap_or(min_date_added.to_string()),
+                            entry
+                                .render()
+                                .unwrap_or("Failed to render message.".to_string()),
+                        )
+                    }))
+                    .chain(block_logs.into_iter().map(|entry| {
+                        (
+                            entry.get_date_added().to_string(),
+                            entry
+                                .render()
+                                .unwrap_or("Failed to render block message.".to_string()),
+                        )
+                    }))
+                    .collect();
+
+                logmessages.sort();
+                logmessages.reverse();
+                logmessages
+            };
+            let template = LogForm {
+                logmessages: logmessages.into_iter().map(|(_, entry)| entry).collect(),
+            };
+            return template;
+        }
+        Err(error) => {
+            println!("Error parsing selected date: {selected_date}. Err: {error}");
+            return LogForm {
+                logmessages: vec![],
+            };
+        }
     }
 }
 
@@ -123,9 +194,16 @@ fn get_empty_log_form() -> HtmlTemplate<LogForm> {
 #[template(path = "hello.html")]
 struct HelloTemplate;
 
-#[derive(Template)]
+#[derive(Template, Debug)]
 #[template(path = "log_explorer.html")]
 struct LogExplorerTemplate;
+
+#[derive(Template, Debug)]
+#[template(path = "log_explorer_static.html")]
+struct LogExplorerTemplateStatic {
+    selected_date: String,
+    logmessages: Vec<String>,
+}
 
 #[derive(Template)]
 #[template(path = "night_plan.html")]
@@ -135,7 +213,7 @@ struct NighPlanTemplate;
 #[template(path = "eon_report.html")]
 struct EndOfNightReportTemplate;
 
-#[derive(Template)]
+#[derive(Template, Debug)]
 #[template(path = "log_list.html")]
 struct LogForm {
     logmessages: Vec<String>,
